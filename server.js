5a803bcf750e0d3acee9b8ef366ac7348feb035e
midsoune97 <156145296+midsoune97@users.noreply.github.com>
2024-03-04 15:53:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("./settings");
 const http = require("http");
 const app = require("./index");
 const PORTHOST = port || 8080;
-
+let PORT = port || 8000 || 5000 || 3000
 http.createServer(app).listen(PORTHOST, () => {
     console.log(`
         █████╗ ██╗     ██████╗ ██╗███████╗
